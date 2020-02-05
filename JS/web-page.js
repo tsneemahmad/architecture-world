4c1541ed6d75c12848dff68250406fdeb1659779
tsneemahmad <tsneemamarat@gmail.com>
2020-02-05 13:23:15 +0200

    new changes

--- JS/web-page.js
+++ JS/web-page.js
@@ -1,13 +1,52 @@
+
+
+
 var projectsname = prompt('how many projects do you know?');
 console.log(projectsname);
-var thanking;
+var interact;
 
 if (projectsname = 4 ) {
-    thanking = 'good job' ;
+    interact = 'good job' ;
 }
 
 else if (projectsname >= 5 ) {
-    thanking = 'wow, very good!' ;
+    interact = 'wow, very good!' ;
 }
 
-document.write('<h3>'+thanking+'</h3>');
+document.write('<h3>'+interact+'</h3>');
+
+
+var architects = prompt('how many architects do you know?');
+console.log(architects);
+var respond;
+
+if (architects = 4 ) {
+respond = 'good job' ;
+}
+
+else if (architects >= 5 ) {
+respond = 'wow, very good!' ;
+}
+
+
+document.write('<h2>'+respond+'</h2>');
+
+
+
+var survey = prompt('on a scale of 10 how much you are enjoying the website?');
+console.log(survey);
+var thanking;
+    
+if (survey >= 8 ) {
+thanking = 'Thank you very much!' ;
+}
+    
+else if (survey >= 5 && survey<= 7 ) {
+thanking = 'Thank you!' ;
+}
+    
+else if ( survey <= 4 ) {
+thanking = 'Thank you, hope you enjoy next time'
+}
+
+document.write('<h1>'+thanking+'</h1>');
