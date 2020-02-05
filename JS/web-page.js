1f1558bce60e7cbd2e3da2662bf950d30cede89d
tsneemahmad <tsneemamarat@gmail.com>
2020-02-05 09:17:10 +0200

    ceated js file

---
+++ JS/web-page.js
@@ -0,0 +1,13 @@
+var projectsname = prompt('how many projects do you know?');
+console.log(projectsname);
+var thanking;
+
+if (projectsname = 4 ) {
+    thanking = 'good job' ;
+}
+
+else if (projectsname >= 5 ) {
+    thanking = 'wow, very good!' ;
+}
+
+document.write('<h3>'+thanking+'</h3>');
